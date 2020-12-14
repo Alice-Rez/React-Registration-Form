b89d21a36ce244dadbe48ce336e4dda17c20a0c0
Alice-Rez <reznickova.alice@seznam.cz>
2020-12-14 16:18:37 +0100

    login works

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,40 @@
+import Axios from "axios";
 import React, { useState } from "react";
 
 export default function Login(props) {
-  const [data, setData] = useState({});
+  const [loginData, setData] = useState({});
 
   return (
     <div className="container">
       <h2 className="display-4 text-info py-3 text-left">Login</h2>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("request send", loginData);
+          Axios({
+            method: "POST",
+            url: "http://localhost:3500/users/login",
+            data: loginData,
+          })
+            .then((res) => {
+              if (res.data.logged) {
+                props.setIsLogged(true);
+                props.setLoggedUser(res.data.uname);
+              }
+            })
+            .catch((err) => console.log(err));
+        }}
+      >
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
           <input
             type="mail"
-            name="mail"
+            name="email"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             onInput={(e) => {
-              setData({ ...data, [e.target.name]: e.target.value });
+              setData({ ...loginData, [e.target.name]: e.target.value });
             }}
           />
           <small id="emailHelp" className="form-text text-muted">
@@ -26,12 +44,12 @@ export default function Login(props) {
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
           <input
-            type="pwd"
-            name="pwd"
+            type="password"
+            name="password"
             className="form-control"
             id="exampleInputPassword1"
             onInput={(e) => {
-              setData({ ...data, [e.target.name]: e.target.value });
+              setData({ ...loginData, [e.target.name]: e.target.value });
             }}
           />
         </div>
@@ -46,22 +64,7 @@ export default function Login(props) {
           </label>
         </div>
         <div className="text-right">
-          <button
-            type="submit"
-            className="btn btn-success btn-lg"
-            onClick={(e) => {
-              e.preventDefault();
-              props.users.map((user) => {
-                console.log(user.mail, user.pwd);
-                if (user.mail === data.mail && user.pwd === data.pwd) {
-                  console.log("condition is true");
-                  props.setIsLogged(true);
-                  props.setLoggedUser(user.name);
-                }
-                return "";
-              });
-            }}
-          >
+          <button type="submit" className="btn btn-success btn-lg">
             Submit
           </button>
         </div>
